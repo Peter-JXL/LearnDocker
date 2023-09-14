32a65310797b9fe7935f05d97932d21f114c89ec
peterjxl <peterjxl@qq.com>
2023-09-14 21:19:44 +0800

    Docker微服务实战

---
+++ src/test/java/com/peterjxl/learndocker/LearnDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterjxl.learndocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
