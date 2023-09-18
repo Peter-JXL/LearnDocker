509cbdb84aec47602c55a5b43ca152a9d48236bd
peterjxl <peterjxl@qq.com>
2023-09-18 11:46:18 +0800

    Docker编排实战项目

--- src/main/java/com/peterjxl/learndocker/LearnDockerApplication.java
+++ src/main/java/com/peterjxl/learndocker/LearnDockerApplication.java
@@ -2,8 +2,10 @@ package com.peterjxl.learndocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.peterjxl.learndocker.mapper")
 public class LearnDockerApplication {
 
     public static void main(String[] args) {
